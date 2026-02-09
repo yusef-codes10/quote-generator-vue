865c65cceaf7c1414a304704562b25a9325187e3
Youcef <youcefcopy@gmail.com>
2026-02-09 09:25:23 +0100

    ninjas api only return one quote at at time | we do not need onMounted since the fetch is related to user click

--- src/components/QMain.vue
+++ src/components/QMain.vue
@@ -7,104 +7,108 @@ const author = ref('Mark Twain')
 // const quote = ref('')
 // const author = ref('')
 
-const quotes = ref([
-  {
-    index: 1,
-    quote: 'Imagination is more important than knowledge.',
-    author: 'Albert Einstein',
-  },
-  {
-    index: 2,
-    quote: 'Science is a way of thinking much more than it is a body of knowledge.',
-    author: 'Carl Sagan',
-  },
-  {
-    index: 3,
-    quote: 'If you want to find the secrets of the universe, think in terms of energy.',
-    author: 'Nikola Tesla',
-  },
-  {
-    index: 4,
-    quote: 'What we know is a drop, what we don’t know is an ocean.',
-    author: 'Isaac Newton',
-  },
-  {
-    index: 5,
-    quote: 'In matters of conscience, the law of the majority has no place.',
-    author: 'Mahatma Gandhi',
-  },
-  {
-    index: 6,
-    quote: 'Those who deny freedom to others deserve it not for themselves.',
-    author: 'Abraham Lincoln',
-  },
-  {
-    index: 7,
-    quote: 'Power tends to corrupt, and absolute power corrupts absolutely.',
-    author: 'Lord Acton',
-  },
-  {
-    index: 8,
-    quote: 'The good thing about science is that it’s true whether or not you believe in it.',
-    author: 'Neil deGrasse Tyson',
-  },
-  {
-    index: 9,
-    quote: 'Man is condemned to be free.',
-    author: 'Jean-Paul Sartre',
-  },
-  {
-    index: 10,
-    quote: 'A lie told often enough becomes the truth.',
-    author: 'Vladimir Lenin',
-  },
-  {
-    index: 11,
-    quote: 'Be the change that you wish to see in the world.',
-    author: 'Mahatma Gandhi',
-  },
-  {
-    index: 12,
-    quote:
-      "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
-    author: 'J.K. Rowling',
-  },
-  {
-    index: 13,
-    quote:
-      "Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.",
-    author: 'Marilyn Monroe',
-  },
-])
+// const quotes = ref([
+//   {
+//     index: 1,
+//     quote: 'Imagination is more important than knowledge.',
+//     author: 'Albert Einstein',
+//   },
+//   {
+//     index: 2,
+//     quote: 'Science is a way of thinking much more than it is a body of knowledge.',
+//     author: 'Carl Sagan',
+//   },
+//   {
+//     index: 3,
+//     quote: 'If you want to find the secrets of the universe, think in terms of energy.',
+//     author: 'Nikola Tesla',
+//   },
+//   {
+//     index: 4,
+//     quote: 'What we know is a drop, what we don’t know is an ocean.',
+//     author: 'Isaac Newton',
+//   },
+//   {
+//     index: 5,
+//     quote: 'In matters of conscience, the law of the majority has no place.',
+//     author: 'Mahatma Gandhi',
+//   },
+//   {
+//     index: 6,
+//     quote: 'Those who deny freedom to others deserve it not for themselves.',
+//     author: 'Abraham Lincoln',
+//   },
+//   {
+//     index: 7,
+//     quote: 'Power tends to corrupt, and absolute power corrupts absolutely.',
+//     author: 'Lord Acton',
+//   },
+//   {
+//     index: 8,
+//     quote: 'The good thing about science is that it’s true whether or not you believe in it.',
+//     author: 'Neil deGrasse Tyson',
+//   },
+//   {
+//     index: 9,
+//     quote: 'Man is condemned to be free.',
+//     author: 'Jean-Paul Sartre',
+//   },
+//   {
+//     index: 10,
+//     quote: 'A lie told often enough becomes the truth.',
+//     author: 'Vladimir Lenin',
+//   },
+//   {
+//     index: 11,
+//     quote: 'Be the change that you wish to see in the world.',
+//     author: 'Mahatma Gandhi',
+//   },
+//   {
+//     index: 12,
+//     quote:
+//       "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+//     author: 'J.K. Rowling',
+//   },
+//   {
+//     index: 13,
+//     quote:
+//       "Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.",
+//     author: 'Marilyn Monroe',
+//   },
+// ])
+const quotes = ref([])
+
+// const generateQuote = () => {
+//   const randomIndex = Math.floor(Math.random() * quotes.value.length)
+//   const randomQuote = quotes.value[randomIndex]
+//   quote.value = randomQuote.quote
+//   author.value = randomQuote.author
+// }
 
 const generateQuote = () => {
-  const randomIndex = Math.floor(Math.random() * quotes.value.length)
-  const randomQuote = quotes.value[randomIndex]
-  quote.value = randomQuote.quote
-  author.value = randomQuote.author
+  // fetch therandom quotes
+  onMounted(async () => {
+    try {
+      // fetch
+      const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+        headers: {
+          'X-Api-Key': import.meta.env.VITE_API_NINJAS_KEY,
+        },
+      })
+
+      // turn into json
+      const data = await response.json()
+
+      // map the data
+      quotes.value = data.map((quote) => ({
+        text: quote.quote,
+        author: quote.author,
+      }))
+    } catch (error) {
+      console.log(error)
+    }
+  })
 }
-// fetch therandom quotes
-onMounted(async () => {
-  try {
-    // fetch
-    const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
-      headers: {
-        'X-Api-Key': import.meta.env.VITE_API_NINJAS_KEY,
-      },
-    })
-
-    // turn into json
-    const data = await response.json()
-
-    // map the data
-    quotes.value = data.map((quote) => ({
-      text: quote.quote,
-      author: quote.author,
-    }))
-  } catch (error) {
-    console.log(error)
-  }
-})
 </script>
 
 <template>
